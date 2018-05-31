f42e55bd145e7becee90ae3d9625d4b6f32e92c5
TassoStamboulis <tasso.stamboulis@gmail.com>
2018-05-31 15:09:04 -0400

    first git commit from intellij! :o

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,12 +22,13 @@ public class Main {
         String userInput = in.nextLine();
 
         //remove all punctuation
-        String replace1 = userInput.replace(',',' ');
-        String replace2 = userInput.replace(':',' ');
-        String replace3 = userInput.replace('!',' ');
+        String replace1 = userInput.replace(',','\0');
+        String replace2 = replace1.replace(':','\0');
+        String replace3 = replace2.replace('!','\0');
+        String noSpace = replace3.replace(' ' ,'\0');
 
         // Convert to lower case to simplify comparing strings
-        String phrase = original.toLowerCase();
+        String phrase = noSpace.toLowerCase();
 
         String backwards = "";
 
@@ -38,14 +39,18 @@ public class Main {
             // and build the backwards string
             String letter = phrase.substring(i, i + 1);
             backwards += letter;
+            System.out.println(letter);
         }
+
+        System.out.println(phrase);
+        System.out.println(backwards);
 
         // A palindrome is a word or phrase that is the same forward or
         // backward. This is where we check that.
         if (phrase.equals(backwards)) {
-            System.out.println(original + " is a palindrome!");
+            System.out.println(userInput + " is a palindrome!");
         } else {
-            System.out.println(original + " is not a palindrome!");
+            System.out.println(userInput + " is not a palindrome!");
         }
 
     }
