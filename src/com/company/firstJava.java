4dd0f603493b5b5c410078b6c2ad6703affbe0a1
TassoStamboulis <tasso.stamboulis@gmail.com>
2018-05-31 16:01:25 -0400

    third commit

---
+++ src/com/company/firstJava.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class firstJava {
+        public static void main( String[] args ) {
+            System.out.println( "Hello, World!" );
+            System.out.println("Today is May 31st, 2018. I am alive!"); }
+
+}
