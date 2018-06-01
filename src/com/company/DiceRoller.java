98f3c7be4438b9a178603b687d4fb0c856e58cad
TassoStamboulis <tasso.stamboulis@gmail.com>
2018-06-01 10:55:11 -0400

    12th commit

---
+++ src/com/company/DiceRoller.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Random;
+
+public class DiceRoller
+{
+    public static void main( String[] args )
+    {
+        int dienumber;
+        Random rnd = new Random();
+
+                dienumber = rnd.nextInt(6) + 1;
+
+                        System.out.println("Your die roll was : " + dienumber);
+    }
+}
